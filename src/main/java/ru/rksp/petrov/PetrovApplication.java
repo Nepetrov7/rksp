77e2170fd68148f03b791f4e20de4e052bba275d
n.petrov <n.petrov@dcoa.ru>
2025-10-21 14:45:00 +0300

    initial project

---
+++ src/main/java/ru/rksp/petrov/PetrovApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.petrov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetrovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetrovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/petrov/PetrovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.petrov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetrovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
